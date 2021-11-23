3693bf50ac051bccc53d35de8c6519c044933d99
AthosF <athoseng@yahoo.com.br>
2021-11-22 21:49:24 -0300

    Primeiro Script externo

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// tipos primitivos
+
+//boolean
+var vOuF = false;
+console.log(vOuF);
+console.log(typeof(vOuF));
+
+// number
+var numeroQualquer = 1;
+console.log(numeroQualquer);
+console.log(typeof(numeroQualquer));
+
+// string
+var nome= "Athos";
+console.log(nome);
+console.log(typeof(nome));
+
+
+
